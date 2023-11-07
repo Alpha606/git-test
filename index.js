eea21f33f4cf2bd79342e03e01e115a2ffa5dfa4
Alpha606 <joshiadi100@gmail.com>
2023-11-07 06:16:40 +0530

    added a simple project like thing to experiment with

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ function add7(num){
      return parseInt(num) + 20;
 }
  console.log(add7("7"));
+
+ function multiply(){
+      let a=document.getElementById("n1").value;
+      let b=document.getElementById("n2").value;
+      let c= parseInt(a*b);
+      alert("The provided multiplicatipn value for the number is "+ c);
+ }
